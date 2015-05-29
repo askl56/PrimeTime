c325a14ed224be779fe7102e5d0585c0789ad6e3
askl56 <amscott@openmailbox.org>
2015-05-29 04:46:22 +0100

    added class

--- lib/prime_time.rb
+++ lib/prime_time.rb
@@ -1,52 +1,59 @@
-# Firstly a function to determine if a given number is Prime.
+class PrimeTime
 
-def prime?(n = 10)
-  (2..n - 1).to_a.all? { |num| n % num != 0 }
-end
-
-# Secondly store the prime numbers in an array.
-
-def prime_storage(n = 10)
-  prime_array = []
-  count = 2
-  while prime_array.length < n
-    prime_array << (count) if prime?(count)
-    count += 1
+  def initialize(i)
+    print_table(i)
   end
-  prime_array
-end
 
-# Thirdly multiply up the primes to create the table
+  # Firstly a function to decipher if a given number is Prime.
 
-def times_the_primes(n = 10)
-  primes = prime_storage(n)
-  primes.unshift(1)
-  multi = []
-  primes.map do |prime|
-    row = []
-    primes.map { |multi| row << (prime * multi) }
-    multi << row
+  def prime?(n = 10)
+    (2..n - 1).to_a.all? { |num| n % num != 0 }
   end
-  multi
-end
 
-# Print the table
+  # Secondly store the prime numbers in an array.
 
-def print_table(n = 10)
-  matrix = times_the_primes(n)
-  matrix.each do |row|
-    row.each do |cell|
-      cell = ' ' if cell == 1
-      print cell.to_s.rjust(5)
+  def prime_storage(n = 10)
+    prime_array = []
+    count = 2
+    while prime_array.length < n
+      prime_array << (count) if prime?(count)
+      count += 1
     end
-    puts "\n"
+    prime_array
+  end
+
+  # Third, multiply up the primes to create the table
+
+  def times_the_primes(n = 10)
+    primes = prime_storage(n)
+    primes.unshift(1)
+    multi = []
+    primes.map do |prime|
+      row = []
+      primes.map { |multi| row << (prime * multi) }
+      multi << row
+    end
+    multi
+  end
+
+  # Print the table
+
+  def print_table(n = 10)
+    matrix = times_the_primes(n)
+    matrix.each do |row|
+      row.each do |cell|
+        cell = ' ' if cell == 1
+        print cell.to_s.rjust(5)
+      end
+      puts "\n"
+    end
   end
 end
 
 # Respond to --count without using OptionParser
 
 if ARGV[0] == '--count'
-  print_table ARGV[1].to_i
+  PrimeTime.new ARGV[1].to_i
 else
-  print_table 10
+  PrimeTime.new 10
 end
--- readme.md
+++ readme.md
@@ -10,7 +10,9 @@ Usage
 -----
 
 Clone the repo.
+
 Run ```bundle install```
+
 To run the program, run ```ruby lib/prime_time.rb```
 
 By default the app will return a 10 column and row times table of prime numbers. However, by appending ```--count <integer>``` and replacing n with an integer of your choosing, you can make the table as large or as small as you like. 
