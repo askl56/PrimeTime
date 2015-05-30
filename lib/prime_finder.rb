0243bb6e95f3f0dd9f56be57aa8959967208bcfc
askl56 <amscott@openmailbox.org>
2015-05-30 01:51:46 +0100

    hard at work refactoring

--- lib/prime_finder.rb
+++ lib/prime_finder.rb
@@ -1,12 +1,12 @@
 class PrimeFinder
 
-  # We wanted to find as simple as possible a way to find all primes and
-  # then to store the primes in an array. Whilst initially we wanted to
+  # I wanted to find as simple as possible a way to find all primes and
+  # then to store the primes in an array. Whilst initially I wanted to
   # have a prime? method, followed by a method which iterates over a range
   # and stores the prime numbers in an array, in the course of the refactoring
-  # process we realised that it is possible to do both.
+  # process I realised that it is possible to do both.
 
-  # Equally we wanted to obey the single responsibility principle, and to make
+  # Equally I wanted to obey the single responsibility principle, and to make
   # the code as simple as possible.
 
   def self.find_primes(n = 10)
@@ -19,15 +19,17 @@ class PrimeFinder
     primes.first(n)
   end
 
+  # As you can see, the method uses the Lazy emunerator to iterate up to
+  # infinity, but without impacting performance. It is then simply a case
+  # of collecting the desired number of primes into an array.
+
+  # This method multiplies the primes together in order to generate an array
+  # of the multiplied values.
+
   def self.times_primes(n = 10)
     primes = find_primes(n)
     primes.unshift(1)
     multi = []
-    primes.map do |prime|
-      row = []
-      primes.map { |multi| row << (prime * multi) }
-      multi << row
-    end
-    multi
+    multi = primes.map {|prime1| primes.map {|prime2| prime2 * prime1 } }
   end
 end
