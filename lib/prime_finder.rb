55339188199f8a73f81958b6b556bf0eb04787e1
askl56 <amscott@openmailbox.org>
2015-05-31 04:02:52 +0100

    modularized, and added fully working tests for the grid

--- lib/prime_finder.rb
+++ lib/prime_finder.rb
@@ -1,15 +1,20 @@
-class PrimeFinder
+module PrimeFinder
+
+  # I deliberated for a while on whether to use a class or an object for this.
+  # I decided that since these are purely behavioural helper methods and nothing
+  # is instantiated, it make more sense to be a module.
+
+  module_function
 
   # I wanted to find as simple as possible a way to find all primes and
-  # then to store the primes in an array. Whilst initially I wanted to
+  # then to store the primes in an array. Whilst initially I wanted to
   # have a prime? method, followed by a method which iterates over a range
-  # and stores the prime numbers in an array, in the course of the refactoring
+  # and stores the prime numbers in an array, in the course of the refactoring
   # process I realised that it is possible to do both.
 
-  # Equally I wanted to obey the single responsibility principle, and to make
-  # the code as simple as possible.
 
-  def self.find_primes(n = 10)
+
+  def find_primes(n = 10)
     infinity = 1.0 / 0
 
     primes = (2..infinity).lazy.select do |i|
@@ -21,16 +26,17 @@ class PrimeFinder
 
   # As you can see, the method uses the Lazy emunerator class method
   # to iterate up to infinity, but without impacting performance. It is
-  # then simply a case of collecting the desired number of primes into an
+  # then simply a case of collecting the desired number of primes into an
   # array.
 
-  def self.times_primes(n = 10)
+  def times_primes(n = 10)
     primes = find_primes(n)
     primes.unshift(1)
     multi = primes.map { |prime1| primes.map {|prime2| prime2 * prime1 } }
   end
 
   # This method multiplies the primes together in order to generate an array
-  # of the multiplied values.
+  # of the multiplied values. I stacked the map functions to make it more
+  # semantic.
 
 end
--- spec/lib/prime_grid_spec.rb
+++ spec/lib/prime_grid_spec.rb
@@ -1,5 +1,95 @@
-require 'rspec'
-require 'prime_grid'
+class PrimeGrid
 
-context "it tests the Prime Grid class" do
+  attr_reader :formatter, :display
+
+  def initialize(formatter, display)
+    @formatter = formatter
+    @display = display
+  end
+
+  def render(matrix)
+    matrix.each_with_index do |row, i|
+      display.display_row formatter.render_separator unless i == 0
+      display.display_row formatter.render_row(row)
+    end
+  end
+end
+
+describe PrimeGrid do
+
+  subject(:a_prime_grid) { PrimeGrid.new(a_formatter, a_display) }
+
+  let(:a_formatter) { double("TextDisplayFormatter").as_null_object }
+  let(:a_display) { double("ConsoleDisplay").as_null_object }
+
+  describe "rendering an empty matrix" do
+    let(:empty_matrix) { [] }
+
+    it "formats nothing" do
+      expect(a_formatter).not_to receive(:render_separator)
+      expect(a_formatter).not_to receive(:render_row)
+
+      a_prime_grid.render(empty_matrix)
+    end
+
+    it "displays nothing" do
+      expect(a_display).not_to receive(:display_row)
+
+      a_prime_grid.render(empty_matrix)
+    end
+  end
+
+  context "rendering a single row matrix" do
+    let(:a_row) { generate_row(_arbitrary_length = 5) }
+    let(:single_row_matrix) { [a_row] }
+
+    it "formats the row" do
+      expect(a_formatter).to receive(:render_row).with(a_row)
+
+      a_prime_grid.render(single_row_matrix)
+    end
+
+    it "does not format a separator" do
+      expect(a_formatter).not_to receive(:render_separator)
+
+      a_prime_grid.render(single_row_matrix)
+    end
+
+    it "displays the row" do
+      allow(a_formatter).to receive(:render_row).and_return(:formatted)
+      expect(a_display).to receive(:display_row).with(:formatted)
+
+      a_prime_grid.render(single_row_matrix)
+    end
+  end
+
+  context "rendering a multi-row matrix" do
+    let(:multiple_rows) { Array.new(5) { generate_row(_arbitrary_length = 5) } }
+    let(:multi_row_matrix) { [multiple_rows] }
+
+    it "formats the row" do
+      expect(a_formatter).to receive(:render_row).with(multiple_rows)
+
+      a_prime_grid.render(multi_row_matrix)
+    end
+
+    it "does not format a separator" do
+      expect(a_formatter).not_to receive(:render_separator)
+
+      a_prime_grid.render(multi_row_matrix)
+    end
+
+    it "displays the rows" do
+      allow(a_formatter).to receive(:render_row).and_return(:formatted)
+      expect(a_display).to receive(:display_row).with(:formatted)
+
+      a_prime_grid.render(multi_row_matrix)
+    end
+  end
+
+  ARBITRARY_MAX = 10
+
+  def generate_row(length)
+    Array.new(length) { Random.rand(ARBITRARY_MAX) }
+  end
 end
